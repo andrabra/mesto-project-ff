b9290fcde9f5f9f472c52e2d0b1c731304c88e94
Andrew Abramov <andrabragames@gmail.com>
2024-02-20 18:23:44 +0500

    необходимо добавить реализацию обхода массива через foreach

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,41 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector("#card-template").content; //находим шаблон карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector(".places__list"); //находим узел куда добавлять карточки
+const cardContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-function addCard(link, name) {
-  const cardItemTemplate = cardTemplate.querySelector(".card").cloneNode(true); //клонируем шаблон карточки
-  const deleteButton = cardItemTemplate.querySelector(".card__delete-button"); //находим кнопку удаления карточки
-  const cardLink = cardItemTemplate.querySelector(".card__image");//записываем узел изображения в переменную
-  const cardImg = cardItemTemplate.querySelector(".card__title");//записываем узел подписи в переменную
+function createCard(imgSrc, title, deleteFnc) {
+  const cardTemplateClone = cardTemplate.querySelector(".card").cloneNode(true);
 
-  cardLink.src = link; //заполняем атрибут изображения
-  cardImg.textContent = name; //заполняем текстовое содержимое 
+  const deleteButton = cardTemplateClone.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteFnc);
 
-  placesList.append(cardItemTemplate); //добавляем заполненный шаблон к списку
+  ////Заполнение карточек
+  const cardImg = cardTemplateClone.querySelector(".card__image");
+  const cardTitle = cardTemplateClone.querySelector(".card__title");
 
-  deleteButton.addEventListener("click", deleteCard); //добавляем слушатель клика на кнопку удаления
+  cardImg.src = imgSrc;
+  cardTitle.textContent = title;
+  ////
+  return cardTemplateClone;
 }
+
 // @todo: Функция удаления карточки
 function deleteCard(evt) {
-  const trash = evt.target.closest(".places__item"); //находим ближайшего родителя, в котором есть кнопка "удалить"
-  trash.remove();
+  console.log(evt.currentTarget);
+  evt.target.closest(".places__item").remove(); //находим ближайшего родителя, в котором есть кнопка "удалить"
 }
 // @todo: Вывести карточки на страницу
-function addContent() {
-  initialCards.forEach(function (item) {
-    addCard(item.link, item.name);//обходим весь массив объектов в cards.js, каждый объект передаем в функцию добавления карточек на страницу
-  });
+function displayCard(arrCards) {
+  // @todo переделать реализацию через foreach
+
+  //реализация через for
+  for (let i = 0; i <= arrCards.length - 1; i++) {
+    cardContainer.append(
+      createCard(arrCards[i].link, arrCards[i].name, deleteCard)
+    );
+  }
 }
 
-addContent();//вызываем эту функцию для инициализации карточек на странице
+displayCard(initialCards);
