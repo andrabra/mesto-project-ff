6bcd79429935c566498f579db23d5071a119c053
Andrew Abramov <andrabra@yandex.ru>
2024-03-14 02:08:25 +0500

    working on a smooth pop-up opening/closing

---
+++ src/components/card.js
@@ -0,0 +1,24 @@
+import { cardTemplate } from "../index.js";
+
+// @todo: Функция создания карточки
+export function createCard(imgSrc, title, deleteFnc) {
+  const cardTemplateClone = cardTemplate.querySelector(".card").cloneNode(true); //выбираю карточку из template и клонирую её
+
+  const deleteButton = cardTemplateClone.querySelector(".card__delete-button"); //выбираю кнопку из копии карточки
+
+  deleteButton.addEventListener("click", () =>  deleteFnc(cardTemplateClone));
+
+  const cardImg = cardTemplateClone.querySelector(".card__image"); //выбираю из клона тег изображения
+  const cardTitle = cardTemplateClone.querySelector(".card__title"); //выбираю из клона тег тайтла
+
+  cardImg.src = imgSrc; //заполняю из аргумента
+  cardImg.alt = title; //заполняю alt
+  cardTitle.textContent = title; //заполняю из аргумента
+
+  return cardTemplateClone; //возвращаю заполненную карточку
+}
+
+// @todo: Функция удаления карточки
+export function deleteCard(card) {
+  card.remove();
+}
--- src/index.js
+++ src/index.js
@@ -1,56 +1,37 @@
-import '../pages/index.css';
-import {initialCards} from '../src/cards.js'
+import "../pages/index.css";
+import { initialCards } from "../src/cards.js";
+import { createCard, deleteCard } from "../src/components/card.js";
+import { openModal, closeModal } from "../src/components/modal.js";
 
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector("#card-template").content; //записываю содержимое блока template
-
+export const cardTemplate = document.querySelector("#card-template").content; //записываю содержимое блока template
 // @todo: DOM узлы
 const cardContainer = document.querySelector(".places__list"); //записываю место куда буду вставлять карточки
 
-// @todo: Функция создания карточки
-function createCard(imgSrc, title, deleteFnc) {
-  const cardTemplateClone = cardTemplate.querySelector(".card").cloneNode(true); //выбираю карточку из template и клонирую её
+export const editBtn = document.querySelector(".profile__edit-button");
+export const popupTypeEdit = document.querySelector(".popup_type_edit");
+export const popup = document.querySelector(".popup");
+export const popupClose = document.querySelector(".popup__close");
 
-  const deleteButton = cardTemplateClone.querySelector(".card__delete-button"); //выбираю кнопку из копии карточки
-//   deleteButton.addEventListener("click", deleteFnc); //добавляю слушатель с функцией удаления на кнопку
+editBtn.addEventListener("click", () => {
+  openModal(popupTypeEdit);
+});
 
-  deleteButton.addEventListener("click", () =>  deleteFnc(cardTemplateClone));
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closeModal(popup);
+  }
+});
 
-
-  const cardImg = cardTemplateClone.querySelector(".card__image"); //выбираю из клона тег изображения
-  const cardTitle = cardTemplateClone.querySelector(".card__title"); //выбираю из клона тег тайтла
-
-  cardImg.src = imgSrc; //заполняю из аргумента
-  cardImg.alt = title; //заполняю alt
-  cardTitle.textContent = title; //заполняю из аргумента
-
-  return cardTemplateClone; //возвращаю заполненную карточку
-}
-
-// @todo: Функция удаления карточки
-function deleteCard(card) {
-//   evt.target.closest(".places__item").remove(); //находим ближайшего родителя, в котором есть кнопка "удалить"
-  card.remove();
-}
+popupClose.addEventListener("mousedown", () => {
+  closeModal(popup);
+});
 
 // @todo: Вывести карточки на страницу
 function displayCard(arrCards) {
-  //// реализация через стрелочную функцию
-  //   arrCards.forEach((item) => {
-  //     cardContainer.append(createCard(item.link, item.name, deleteCard))
-  //   });
-
   arrCards.forEach(function (item) {
     cardContainer.append(createCard(item.link, item.name, deleteCard));
   });
-  ////реализация через for
-  //   for (let i = 0; i <= arrCards.length - 1; i++) {
-  //     cardContainer.append(
-  //       createCard(arrCards[i].link, arrCards[i].name, deleteCard)
-  //     );
-  //   }
 }
-
-
 
 displayCard(initialCards);
